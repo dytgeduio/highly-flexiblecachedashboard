23ec2f57711303b68e5782653f7bdd2ff04fd22f
dytgeduio <hsjchjjjjjjjjbb@hotmail.com>
2024-05-07 14:41:54 +0700

    commit

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
-  }
-  return arr;
-}
+const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);

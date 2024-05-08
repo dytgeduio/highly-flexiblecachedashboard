eae999fabb54c4d3f7c2713a58b6886b1f2f3e58
dytgeduio <hsjchjjjjjjjjbb@hotmail.com>
2024-05-09 02:51:44 +0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+}

9aa11f9bf8d84a57a24ea701df3af8b8c49dc533
dytgeduio <hsjchjjjjjjjjbb@hotmail.com>
2024-05-14 13:12:26 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-function threeSum(nums) {
-  nums.sort((a, b) => a - b);
-  const result = [];
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
-      let low = i + 1;
-      let high = nums.length - 1;
-      const sum = 0 - nums[i];
-      while (low < high) {
-        if (nums[low] + nums[high] === sum) {
-          result.push([nums[i], nums[low], nums[high]]);
-          while (low < high && nums[low] === nums[low + 1]) low++;
-          while (low < high && nums[high] === nums[high - 1]) high--;
-          low++;
-          high--;
-        } else if (nums[low] + nums[high] < sum) low++;
-        else high--;
+function isAlienSorted(words, order) {
+  const dict = new Map();
+  for (let i = 0; i < order.length; i++) {
+    dict.set(order[i], i);
+  }
+  for (let i = 0; i < words.length - 1; i++) {
+    const word1 = words[i];
+    const word2 = words[i + 1];
+    let found = false;
+    for (let j = 0; j < Math.min(word1.length, word2.length); j++) {
+      if (word1[j] !== word2[j]) {
+        if (dict.get(word1[j]) > dict.get(word2[j])) return false;
+        found = true;
+        break;
       }
     }
+    if (!found && word1.length > word2.length) return false;
   }
-  return result;
+  return true;
 }
